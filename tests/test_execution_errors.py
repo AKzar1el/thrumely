8e24c67f7220ba71d9aa778bca32481bdd17aecd
Tommy <104433268+AKzar1el@users.noreply.github.com>
2026-09-02 00:54:26 +0200

    test: narrow shared errors to generic calibration adapters

--- tests/test_execution_errors.py
+++ tests/test_execution_errors.py
@@ -5,21 +5,9 @@ from thrumely.interfaces import (
     ControllerProtocolError,
     ProviderExecutionError,
 )
-from thrumely.bfl_provider import BFLProviderExecutionError
-from thrumely.google_provider import GoogleProviderExecutionError
-from thrumely.openai_provider import ProviderExecutionError as OpenAIProviderExecutionError
-from thrumely.openai_controller import (
-    ControllerExecutionError as OpenAIControllerExecutionError,
-    ControllerProtocolError as OpenAIControllerProtocolError,
-)
 
 
-def test_provider_errors_share_one_calibration_boundary() -> None:
-    assert OpenAIProviderExecutionError is ProviderExecutionError
-    assert issubclass(BFLProviderExecutionError, ProviderExecutionError)
-    assert issubclass(GoogleProviderExecutionError, ProviderExecutionError)
-
-
-def test_controller_errors_share_one_calibration_boundary() -> None:
-    assert OpenAIControllerExecutionError is ControllerExecutionError
-    assert OpenAIControllerProtocolError is ControllerProtocolError
+def test_generic_execution_errors_are_typed_runtime_boundaries() -> None:
+    assert issubclass(ProviderExecutionError, RuntimeError)
+    assert issubclass(ControllerExecutionError, RuntimeError)
+    assert issubclass(ControllerProtocolError, RuntimeError)
